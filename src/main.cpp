48b597e31a2a0db580f7cc87e29927321051cc48
Z-bin <1134636325@qq.com>
2022-09-01 22:48:07 +0800

    feat: 初始化QML窗口

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
+#include <QQmlContext>
 #include <QApplication>
 
 #include <QQuickView>
@@ -16,6 +18,17 @@ int main(int argc, char *argv[])
     // requires the LC_NUMERIC category to be set to "C", so change it back.
     std::setlocale(LC_NUMERIC, "C");
 
+    QQuickStyle::setStyle(QStringLiteral("org.kde.desktop"));
+    QQuickStyle::setFallbackStyle(QStringLiteral("Fusion"));
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+        if (!obj && url == objUrl)
+            QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(url);
 
     app.exec();
 }
---
+++ src/qml/main.qml
@@ -0,0 +1,17 @@
+import QtQuick 2.12
+import QtQuick.Controls 2.12
+import QtQuick.Window 2.12
+import QtQuick.Layouts 1.12
+import QtQuick.Dialogs 1.2
+import QtGraphicalEffects 1.12
+import Qt.labs.settings 1.0
+
+
+ApplicationWindow {
+    id: window
+
+    visible: true
+    title:  qsTr("Haruna")
+    width: 1280
+    height: 720
+}
